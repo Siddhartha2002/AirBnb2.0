5b4f0c11c1e5d1bb4f8a5a3cc9479b82510e5fb2
Siddhartha2002 <siddharthashukla02@gmail.com>
2022-01-10 19:30:46 +0530

    FULL AIRBNB COMPLETE

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoic2lkZGhhcnRoYTIwMDIiLCJhIjoiY2t5OG54YXZxMTEwaTJvbjQwZTIzMjY3ZCJ9.8-M8v7ZSpDwRXInqfKo3ig",
+  },
+};
